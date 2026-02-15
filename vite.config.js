2d0d2e6f3616f4e373ab776ec9f26b1d15e97be8
AndrewTJohnston <andrew@andrewtjohnston.com>
2026-02-14 20:49:53 -0500

    Updated vite.config.js with new configuration settings

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import tailwindcss from '@tailwindcss/vite'
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
-})
+export default {
+  build: {
+    outDir: 'dist',
+  },
+  base: '/intractableventures-dot-com/',
+}
